aaaaeb5de41180c15259a2844ab228bde01fc935
xstefank <xstefank122@gmail.com>
2025-06-26 17:14:36 +0200

    More critical error message for low battery

--- bin/check_battery.sh
+++ bin/check_battery.sh
@@ -2,6 +2,6 @@
 
 BATTINFO=`acpi -b`
 if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:15:00 ]] ; then
-    DISPLAY=:0.0 /usr/bin/notify-send -u critical "low battery" "$BATTINFO"
+    DISPLAY=:0.0 /usr/bin/notify-send -u critical "z kadzi paru zoberem???" "$BATTINFO"
 fi
 
